9e741d1b187801680e1f4dbd28b29b9b71f9f192
nomburlaargonus <60740365+nomburlaargonus@users.noreply.github.com>
2020-02-06 11:12:48 -0300

    ejercicio uno primera clase 6/2/2020
    aprendimos "alert" y "variables"

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,19 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	primerPrecio=""
+	segundoPrecio=""
+	apellido="perez"
+
+	//alert es una variable que muestra texto
+	//caundo en txto esta en "" se muestra eso como texto alert("Esto funciona de maravilla");
+	alert(apellido);
+	//alert("apellido");
+	//alert(); sale basio
+	//alert(primerpri); si la variante no existe sale error 
+}
+
