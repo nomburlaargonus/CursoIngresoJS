36c3ab9b1093f2eadada569ef7c4f49793e952cb
nomburlaargonus <60740365+nomburlaargonus@users.noreply.github.com>
2020-02-06 11:59:25 -0300

    ejercicio console.log 6/2/2020

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
-}
 
+	var nombre;
+	var usuario;
+    nombre= prompt("Porfavor ingrese su nombre", usuario); 
+
+    console.log("ingrese su nombre  " + nombre); //esto es para que solo salga en la consola
+    //alert("ingrese su nombre  " + nombre);
+
+
+}
