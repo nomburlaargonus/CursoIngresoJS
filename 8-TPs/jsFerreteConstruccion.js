ce97869533494dd4d5e6791b8d943bd156b8b189
sebastian c c <60740365+nomburlaargonus@users.noreply.github.com>
2020-02-11 18:18:02 -0300

    tp 11/2

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -7,12 +7,44 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+	var largoT;
+	var anchoT;
+	var multiplicarElLargo;
+	var multiplicarElancho;
+    
+	largoT = document.getElementById('Largo del terreno').value;
+    largoT = parseInt (largoT);
+
+	anchoT = document.getElementById('Ancho del terreno').value;
+    anchoT = parseInt (anchoT);
+
+    multiplicarElLargo = largoT * 3;
+
+    multiplicarElancho = ancho * 3;
+
+    alert("Se necesita " + multiplicarElLargo " para el largo y se necesita" + multiplicarElancho " para el ancho")
+
 }
 function Circulo () 
 {
+    var radioT;
+
+
+	radioT = document.getElementById('Radio del terreno').value;
+    radioT = parseInt (radioT);
 	
 }
 function Materiales () 
 {
+	var largoT;
+	var anchoT;
+
+    largoT = document.getElementById('Largo del terreno').value;
+    largoT = parseInt (largoT);
+
+	anchoT = document.getElementById('Ancho del terreno').value;
+    anchoT = parseInt (anchoT);
+
+
 	
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,76 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var sumar;
+
+	primerPrecio = document.getElementById('PrecioUno').value;
+    primerPrecio = parseInt (primerPrecio);
+
+	segundoPrecio = document.getElementById('PrecioDos').value;
+    segundoPrecio = parseInt (segundoPrecio);
+
+	tercerPrecio = document.getElementById('PrecioTres').value;
+    tercerPrecio = parseInt (tercerPrecio);
+
+	sumar = primerPrecio + segundoPrecio + tercerPrecio;
+
+	alert(sumar);
+
+
 }
 function Promedio () 
 {
-	
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var sumar;
+	var promediar;
+
+	primerPrecio = document.getElementById('PrecioUno').value;
+    primerPrecio = parseInt (primerPrecio);
+
+	segundoPrecio = document.getElementById('PrecioDos').value;
+    segundoPrecio = parseInt (segundoPrecio);
+
+	tercerPrecio = document.getElementById('PrecioTres').value;
+    tercerPrecio = parseInt (tercerPrecio);
+
+    sumar = primerPrecio + segundoPrecio + tercerPrecio;
+
+	promediar = sumar / 3;
+
+	alert(promediar);
 }
 function PrecioFinal () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var sumar;
+	var precioFinal;
+	var IVA;
+	var calcular;
+
+	primerPrecio = document.getElementById('PrecioUno').value;
+    primerPrecio = parseInt (primerPrecio);
+
+	segundoPrecio = document.getElementById('PrecioDos').value;
+    segundoPrecio = parseInt (segundoPrecio);
+
+	tercerPrecio = document.getElementById('PrecioTres').value;
+    tercerPrecio = parseInt (tercerPrecio);
+
+    IVA= 21;
+
+    sumar = primerPrecio + segundoPrecio + tercerPrecio;
+
+    calcular = sumar * IVA/100;
+
+    precioFinal = sumar + calcular;
+
+    alert(precioFinal);
 	
 }
