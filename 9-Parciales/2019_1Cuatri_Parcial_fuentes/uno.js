0ec673c525afaa28efabcf024fd26e3b1f86e6c7
sebastian c c <60740365+nomburlaargonus@users.noreply.github.com>
2020-02-24 16:17:16 -0300

    24/2

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho = prompt("Coloque el ancho del rectangulo");
+	ancho = parseInt(ancho);
+
+	largo = prompt("Coloque el largo del rectangulo");
+	largo = parseInt(largo);
+
+	perimetro = ancho + largo * 2;
+	alert(perimetro+"este es el perimetro del rectangulo");
 
 }
